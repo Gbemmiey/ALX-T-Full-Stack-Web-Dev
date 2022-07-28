ef93e7c71b67c4b3f68d3c4de34c37fa086fcae8
Gbemmiey <gbemfemalabi@gmail.com>
2022-07-28 15:03:51 +0100

    Update: Loading config into dictionary

--- demo.py
+++ demo.py
@@ -1,4 +1,19 @@
+from dotenv import dotenv_values
 import psycopg2
 
-connection = psycopg2.connect(database="demo", user="postgres", password="deft_dev")
+config = dotenv_values()
+
+connection = psycopg2.connect(database = config['DB_NAME'], user = config['USER'], password = config['PASSWORD'])
+
 cursor = connection.cursor()
+cursor.execute('''
+               CREATE TABLE IF NOT EXISTS todos (
+                   id INTEGER PRIMARY KEY,
+                   completed BOOLEAN NOT NULL DEFAULT FALSE); 
+                   ''')
+cursor.execute('INSERT INTO todos (id, completed) VALUES (2, false);')
+
+connection.commit()
+
+cursor.close()
+connection.close()
