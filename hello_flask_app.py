7c596559dbf736bc37f8c48948bdbba0ab38beb4
Gbemmiey <gbemfemalabi@gmail.com>
2022-07-31 14:29:22 +0100

    Update: added flask_sqlalchemy

--- hello_flask_app.py
+++ hello_flask_app.py
@@ -1,8 +1,23 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import dotenv_values
+
+config = dotenv_values()
 
 app = Flask(__name__)
 
+DB_URI = config['DB_SERVER'] + '://' + config['USER'] + ':' + config['PASSWORD'] + \
+    '@' + config['DB_IP'] + ':' + config['DB_PORT'] + '/' + config['DB_NAME']
+
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
+
+db = SQLAlchemy(app)
+
 
 @app.route('/')
-def home():
-    return "hello world"
+def index():
+    return "Hello world"
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", debug=True)
